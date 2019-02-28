0ef6175b2187c07f02b3d8487bb8fe2903790899
Ben Kissi <benkissi111@gmail.com>
2019-02-28 19:51:15 +0000

    feature: add get weather data for a location

---
+++ app/server.js
@@ -0,0 +1,15 @@
+var axios = require('axios')
+
+const WORLD_WEATHER_ONLINE_TIME_URL= 'http://api.worldweatheronline.com/premium/v1/tz.ashx?key=6a7631aed25a4edead8173220192802&format=json'
+const WORLD_WEATHER_ONLINE_WEATHER_URL= 'http://api.worldweatheronline.com/premium/v1/weather.ashx?key=6a7631aed25a4edead8173220192802&format=json'
+
+const getWeatherData = async (location) => {
+
+    const encodedLocation = encodeURIComponent(location)
+    const requestUrl = `${WORLD_WEATHER_ONLINE_WEATHER_URL}&q=${encodedLocation}`
+    const res = await axios.get(requestUrl)
+    return res.data.data;
+    
+}
+
+getWeatherData('london')
