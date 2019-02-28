184ebc1ebd9350744ec28e81b35af25d03683594
Ben Kissi <benkissi111@gmail.com>
2019-02-28 21:20:38 +0000

    feature: add getInfoForLocation function to get both time and weather data

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
 var axios = require('axios')
+const {asyncForEach} = require('./utilities/utilities')
 
 const WORLD_WEATHER_ONLINE_TIME_URL= 'http://api.worldweatheronline.com/premium/v1/tz.ashx?key=6a7631aed25a4edead8173220192802&format=json'
 const WORLD_WEATHER_ONLINE_WEATHER_URL= 'http://api.worldweatheronline.com/premium/v1/weather.ashx?key=6a7631aed25a4edead8173220192802&format=json'
@@ -21,7 +22,24 @@ const getTime =  async (location) => {
 
 }
 
+const getInfoForLocation = async (locations) => {
+    let weatherData = [];
+    await asyncForEach(locations, async (location) => {
+        const weather = await getWeatherData(location.name)
+        const timeInfo = await getTime(location.name)
+        const weatherDetails = {
+            weather,
+            timeInfo
+        }
+        weatherData.push(weatherDetails)        
+    })
+    console.log(weatherData)
+}
+
+getInfoForLocation([{name:'new york', postal_code:'10013'}, {name:'london', postal_code:'WC2N 5DU'}])
+
 module.exports = {
+    getInfoForLocation,
     getWeatherData,
     getTime
 }
