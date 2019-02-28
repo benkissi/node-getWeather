cd9adc3934df4ac10f9c061e6500a86eec408007
Ben Kissi <benkissi111@gmail.com>
2019-02-28 21:04:03 +0000

    test: add test for getLocation function

--- app/test/app.test.js
+++ app/test/app.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect
-const {getTime} = require('../server.js')
+const {getTime, getWeatherData} = require('../server.js')
 
 describe('getTime()', () => {
   it('should get current time info for location',  async () => {
@@ -9,4 +9,13 @@ describe('getTime()', () => {
     expect(res.data.request[0].query).to.equal(location)
   });
 });
+
+describe('getWeather()', () => {
+    it('should get current weather info for location',  async () => {
+      const location = "London"
+      const res = await getWeatherData(location)
+      expect(res).to.exist
+      expect(res.data.current_condition[0].temp_C).to.be.a('string')
+    });
+  });
     
